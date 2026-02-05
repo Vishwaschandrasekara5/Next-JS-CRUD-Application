bafd6449b209ab985c3378678a69ca0894ed7c4a
Vishwaschandrasekara5 <sandunv05@gmail.com>
2026-02-05 20:03:58 +0530

    client pages created

---
+++ src/app/editTopic/[id]/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import EditTopicForm from '../../(components)/EditTopicForm.jsx'
+
+export default function page() {
+  return (
+    <div>
+      <EditTopicForm />
+    </div>
+  )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TopicList from './(components)/TopicList.jsx';
+
+export default function Home() {
+  return (
+   <>
+   <TopicList/>
+   </>
+  );
+}
