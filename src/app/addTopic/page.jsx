970e0bfeb243c864d9070e564395e834096b13b4
Vishwaschandrasekara5 <sandunv05@gmail.com>
2026-02-06 12:35:15 +0530

    topiclist & addtopic created

--- src/app/addTopic/page.jsx
+++ src/app/addTopic/page.jsx
@@ -1,14 +1,58 @@
+'use client';
+
 import React from 'react'
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
 
 export default function page() {
+  const [title,setTitle]=useState("");
+  const [description,setDescription]=useState("");
+    const router = useRouter();
+
+  const handlesubmit = async (e)=>{
+    e.preventDefault();
+
+    if(!title || !description){
+        alert("Please fill all the fields");
+        return;
+    }
+
+    try {
+      const res = await fetch("http://localhost:3000/api/topics",{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({title,description}),
+      });
+
+      if(res.ok){
+        router.push("/");
+      }
+      else{
+        throw new Error("Failed to create topic");
+      }
+      alert("Topic created successfully");
+    } catch (error) {
+      console.log(error);
+      
+      
+    }
+  }
+
   return (
-    <form className="flex flex-col gap-3">
-        <input 
+    <form onSubmit={handlesubmit} className="flex flex-col gap-3">
+        <input
+        onChange={(e)=>setTitle(e.target.value)}
+        value={title} 
         className="border border-slate-500 px-8 py-2"
         type="text"
         placeholder="Topic Title"/>
 
         <input 
+        onChange={(e)=>setDescription(e.target.value)}
+        value={description}
         className="border border-slate-500 px-8 py-2"
         type="text"
         placeholder="Topic Description"/>
